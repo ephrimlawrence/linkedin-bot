678abe83c6f91c6072a8ebddec71bcb0b60d9236
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2026-03-13 07:46:32 +0000

    feat: implement posts display

--- commands/linkedin_poster.ts
+++ commands/linkedin_poster.ts
@@ -2,24 +2,23 @@ import UserTopic from "#models/user_topic";
 import { BaseCommand } from "@adonisjs/core/ace";
 import type { CommandOptions } from "@adonisjs/core/types/ace";
 import { createPost } from "../app/helpers/linkedin_api.ts";
-
 export default class LinkedinPoster extends BaseCommand {
 	static commandName = "linkedin:poster";
 	static description = "";
 
-	static options: CommandOptions = {};
+	static options: CommandOptions = {
+		startApp: true,
+	};
 
 	async run() {
 		this.logger.info("Executing LinkedIn poster command");
-
 		const topics = await UserTopic.all();
 		for (const topic of topics) {
-			const currentDay = new Date().getDay();
-			if ((topic.days as number[]).indexOf(currentDay) === -1) {
-				continue;
-			}
-
-			if ((topic.days as string[]).indexOf(currentDay.toString()) === -1) {
+			const days = (JSON.parse(topic.days) as number[]).map((i) =>
+				i.toString(),
+			);
+			const currentDay = new Date().getDay().toString();
+			if (days.indexOf(currentDay) === -1) {
 				continue;
 			}
 
